b68b5e926f28c9190235b9f84ba5b7be72f3fdf5
Stanley Williamson <stanleywilliamson47@gmail.com>
2021-01-05 18:04:37 -0500

    Add project files.

---
+++ Program.fs
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Welcome to the Lost Opportunity Calculator!"
+    0 // return an integer exit code
